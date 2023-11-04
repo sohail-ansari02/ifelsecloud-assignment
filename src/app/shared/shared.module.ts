b5152c9d6ce5cf498e04249904e54773b93f65d3
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-11-04 17:04:50 +0530

    added font awsom module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -11,6 +12,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     RouterModule,
+    FontAwesomeModule,
   ]
 })
 export class SharedModule { }
