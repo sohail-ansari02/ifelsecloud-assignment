cecb36d093520d0af4f078c709e6e13224c6d449
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-11-05 22:15:13 +0530

    added styling

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent {
-  @Input() disabled: boolean = !false;
+  @Input() disabled: boolean = false;
+  @Input() type: 'button' | 'menu' | 'reset' | 'submit' = 'button';
 
 }
